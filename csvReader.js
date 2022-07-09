bd78af505ff0d0272a9e89a1416a25a95aced7cc
Neel Thakkar <57473461+Neel278@users.noreply.github.com>
2022-07-09 21:03:10 +0530

    csv reader script added

---
+++ csvReader.js
@@ -0,0 +1,24 @@
+// script to add data from a csv file
+function addDataFromCsv() {
+    $(document).ready(async function () {
+        let csvData = await $.get("../files/backend_task_sample_data.csv");
+        let { data } = Papa.parse(csvData, { header: true });
+        for (let val of data) {
+            try {
+                await fetch("http://localhost:5000/insertEvent", {
+                    method: "POST",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ eventData: val }),
+                });
+            } catch (e) {
+                // return false;
+            }
+        }
+    });
+}
+
+// uncomment this to test script
+// addDataFromCsv();
